4650bf4610a02a0f7aefbb943ce979ef4a2f51a3
StarCat <84149668+Star-Cat13@users.noreply.github.com>
2023-08-27 15:50:15 -0600

    rooms

--- components/rooms.js
+++ components/rooms.js
@@ -7,7 +7,7 @@ class Rooms extends HTMLElement {
         this.innerHTML = `
         <div class="row room-pics">
             <div class="column">
-                <img src="https://github.com/Star-Cat13/3BM/blob/main/public/room05-400x284.jpg?raw=true" onclick="openModal(); currentSlide(1);" class="hover-shadow" />
+                <img src="https://github.com/Star-Cat13/3bears/blob/main/public/2023-room-4-bed-400x284.png?raw=true" onclick="openModal(); currentSlide(1);" class="hover-shadow" />
             </div>
             <div class="column">
                 <img src="https://github.com/Star-Cat13/3BM/blob/main/public/room01-400x284.jpg?raw=true" onclick="openModal(); currentSlide(2);" class="hover-shadow" />
@@ -28,7 +28,7 @@ class Rooms extends HTMLElement {
 
                     <div class="mySlides">
                         <div class="numbertext">1 / 4</div>
-                        <img src="https://threebearsmotel.com/wp-content/uploads/2021/06/room05-scaled.jpg" style="width: 100%;"  />
+                        <img src="https://github.com/Star-Cat13/3bears/blob/main/public/2023-room-4-bed.jpg?raw=true" style="width: 100%;"  />
                         
                     </div>
 
@@ -57,7 +57,7 @@ class Rooms extends HTMLElement {
                    
                     <div class='thumbnails-container'>
             <div class="column">
-                <img class="demo" src="https://github.com/Star-Cat13/3BM/blob/main/public/room05-400x284.jpg?raw=true" onclick="currentSlide(1);" alt="" />
+                <img class="demo" src="https://github.com/Star-Cat13/3bears/blob/main/public/2023-room-4-bed-400x284.png?raw=true" onclick="currentSlide(1);" alt="" />
             </div>
             <div class="column">
                 <img class="demo" src="https://github.com/Star-Cat13/3BM/blob/main/public/room01-400x284.jpg?raw=true" onclick="currentSlide(2);" alt="" />
