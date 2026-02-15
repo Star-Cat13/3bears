ea6dadbc65a2f335ab43682d34cb63e7241eb84d
StarCat <wafflesislove@gmail.com>
2026-02-15 12:07:14 -0700

    nav icon

--- components/header.js
+++ components/header.js
@@ -6,10 +6,13 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <div class="topnav" id="myTopnav">
-        <a href="./index.html" class="active">Home</a>
+        <a href="./index.html" class="nav-brand" aria-label="Home">
+            <img class="nav-logo" src="./public/icon.svg" alt="Three Bears Motel" />
+        </a>
+        <a href="./index.html" class="nav-link">Home</a>
 
-        <a href="./contact.html">Contact</a>
-        <a href="./about.html">About</a>
+        <a href="./contact.html" class="nav-link">Contact</a>
+        <a href="./about.html" class="nav-link">About</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
             <i class="fa fa-bars"></i>
         </a>
@@ -17,6 +20,19 @@ class Header extends HTMLElement {
 
     
       `;
+
+        const getBasename = (pathname) => {
+            const parts = String(pathname).split('/').filter(Boolean);
+            return parts[parts.length - 1] || 'index.html';
+        };
+
+        const current = getBasename(window.location.pathname);
+        const links = this.querySelectorAll('.topnav a.nav-link');
+        links.forEach((a) => {
+            const href = a.getAttribute('href') || '';
+            const target = getBasename(new URL(href, window.location.href).pathname);
+            a.classList.toggle('active', target === current);
+        });
     }
 }
 
